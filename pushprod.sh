02e30823a8a55e01a08abad54e744bca5f420baa
Alexandre Derumier <aderumier@gmail.com>
2026-02-25 13:09:38 +0100

    chore: remove pushprod.sh from git tracking and add to .gitignore

--- pushprod.sh
+++
@@ -1,7 +0,0 @@
-bash build-deb.sh
-tailscale up --login-server https://ovpn2.odiso.net --accept-routes --operator=aderumier
-scp -P 8735 batocera-games-catalog_1.0.0_all.deb root@rgs-retro.ddns.net:/root/
-ssh -p 8735 root@rgs-retro.ddns.net 'dpkg -i batocera-games-catalog_1.0.0_all.deb'
-tailscale down
-scp -r services/download_service batocera:/userdata/system/rgs/
-ssh root@batocera '/userdata/system/services/rgs_download stop && /userdata/system/services/rgs_download start'
